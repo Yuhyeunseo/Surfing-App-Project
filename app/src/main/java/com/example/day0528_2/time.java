8d04c2fdb99b9fdbe41d52191f89ae01307db977
Yuhyeunseo <yu7603@gmail.com>
2024-10-03 22:07:14 +0900

    commit

---
+++ app/src/main/java/com/example/day0528_2/time.java
@@ -0,0 +1,50 @@
+package com.example.day0528_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class time extends AppCompatActivity {
+    ImageButton tbtn1, tbtn2;
+    Button homeBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time);
+
+        tbtn1 = findViewById(R.id.tbtn1);
+        tbtn2 = findViewById(R.id.tbtn2);
+        homeBtn = findViewById(R.id.homeBtn);
+
+        tbtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), timer.class);
+                startActivity(intent);
+            }
+        });
+
+        tbtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), stopwatch.class);
+                startActivity(intent);
+            }
+        });
+
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
